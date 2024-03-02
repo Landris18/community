46f5c47089595f5237e4d75131a127dcdda5fefc
Landris18 <landry.aps@gmail.com>
2024-03-02 23:56:41 +0300

    [ADD] Add login page with no api integration

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from '../../assets/images/react.svg'
-import viteLogo from '/vite.svg'
+import { Box, Button, Container, FormControl, Stack, TextField } from '@mui/material'
+import communityLogoDark from "/community-dark.svg"
+import LoginIcon from "@mui/icons-material/Login"
 import './Login.scss'
 
-export default function Login() {
-    const [count, setCount] = useState(0)
 
+export default function Login() {
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <Container maxWidth={false} className={"login-container"}>
+            <Stack justifyContent={"center"} alignItems={"center"} width={"100%"} height={"100%"}>
+                <Stack bgcolor={"white"} p={4} borderRadius={5} my={3}>
+                    <Stack alignItems={"center"} justifyContent={"center"}>
+                        <Box component={"img"} src={communityLogoDark} width={50} />
+                        <h2 className='lexend-bold m-0' >Community</h2>
+                    </Stack>
+                    <Stack gap={1.5} mt={2}>
+                        <FormControl fullWidth>
+                            <TextField label={"Nom d'utilisateur"} variant="outlined" />
+                        </FormControl>
+                        <FormControl fullWidth>
+                            <TextField label={"Mot de passe"} type="password" variant="outlined" />
+                        </FormControl>
+                        <Button variant="contained" className='primary-button' sx={{ mt: 0.8 }} startIcon={<LoginIcon sx={{ color: "white" }} />}>
+                            S'authentifier
+                        </Button>
+                    </Stack>
+                </Stack>
+                <small className='m-0 text-center' style={{lineHeight: 2}}>
+                    Pour toutes questions et problème d'authentification, veuillez contacter <br />
+                    <span className='lexend-bold cursor-pointer' style={{ textDecoration: "underline" }} onClick={() => window.open("https://facebook.com/Landris18", '_blank')}>
+                        Landry Manankoraisina
+                    </span>
+                </small>
+            </Stack>
+        </Container>
     )
 }
